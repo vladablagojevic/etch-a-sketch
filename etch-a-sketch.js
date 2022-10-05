46b2bdcfeae0df21552462654ffa3b2092228ba5
Vladimir Blagojevic <blagojevic.vlada@gmail.com>
2022-10-05 21:40:21 +0200

    Logic for making new grid done
    Logic for making new grid in the same div's place is done. Added formula
    for calculating the grid size and the size of its subelements div's.
    Conditioned the user to enter only numbers from 1 to 100. Added new
    buttons for the rgb colors to be added and added event listener to it to
    wait for the mouseevent when the button is clicked. The same is done for
    the erase buttons. One is erasing everything and the other is erasing
    one square at the time.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,24 +1,86 @@
 let container = document.getElementById('container');
 const blackBtn = document.getElementById('blackBtn');
+const rgbBtn = document.getElementById('rgbBtn');
+const eraseAllBtn = document.getElementById('eraseAllBtn');
 const newGridBtn =  document.getElementById('newGridBtn');
+const eraseOneByOneBtn = document.getElementById('eraseOneByOne');
 let div = null;
 
-for (let i = 1; i < 17; i++) {
-  div = document.createElement('div');
-  div.id = `div${i}`;
-  div.className = 'divs';
-  div.style.width = '25%';
-  div.style.height = '25%';
-  div.style.border = '1px solid rgba(0,0,0,0.25)';
-  container.appendChild(div);
+container.style.display = 'flex';
+container.style.flexWrap = 'wrap';
+container.style.maxWidth = '600px';
+container.style.maxHeight = '600px';
+container.style.width = '100%';
+container.style.height = '100%';
+container.style.boxShadow = '10px 10px 10px 10px';
+
+const gridInit = () => {
+  for (let i = 1; i < 17; i++) {
+    div = document.createElement('div');
+    div.id = `div${i}`;
+    div.className = 'divs';
+    div.style.width = '25%';
+    div.style.height = '25%';
+    div.style.border = '1px solid rgba(0,0,0,0.25)';
+    container.appendChild(div);
+  }
 }
+
+gridInit();
 
 let divs = document.getElementsByClassName('divs');
 
 blackBtn.addEventListener('click', () => {
   Array.from(divs).forEach(element => {
     element.addEventListener('mouseenter', () => {
-      element.style.backgroundColor = 'rgba(0,0,0,0.25)';
+      element.style.backgroundColor = 'rgba(0,0,0,0.85)';
+      // element.classList.add('divsBlack');
     });
   });
+});
+
+rgbBtn.addEventListener('click', () => {
+  Array.from(divs).forEach(element => {
+    const randomRgb1 = Math.floor(Math.random() * 256);
+    const randomRgb2 = Math.floor(Math.random() * 256);
+    const randomRgb3 = Math.floor(Math.random() * 256);
+    element.addEventListener('mouseenter', () => {
+      element.style.backgroundColor = `rgba(${randomRgb1}, ${randomRgb2}, ${randomRgb3})`;
+    });
+  });
+});
+
+eraseAllBtn.addEventListener('click', () => {
+  Array.from(divs).forEach(element => {
+    element.style.backgroundColor = 'white';
+  });
+});
+
+eraseOneByOneBtn.addEventListener('click', () => {
+  Array.from(divs).forEach(element => {
+    element.addEventListener('mouseenter', () => {
+      element.style.backgroundColor = 'white';
+    });
+  });
+});
+
+newGridBtn.addEventListener('click', () => {
+  const gridNr = prompt('Enter number between 1 and 100 for grid cells on each side');
+  container.replaceChildren();
+  if (gridNr > 0 && gridNr < 100) {
+    let widthOfDiv = 600 / gridNr;
+    for (let i = 0; i < gridNr * gridNr; i++) {
+      div = document.createElement('div');
+      div.style.width = `${widthOfDiv}px`;
+      div.style.height = `${widthOfDiv}px`;
+      div.id = `div${i}`;
+      div.className = 'divs';
+      div.style.border = '1px solid rgba(0,0,0,0.25)';
+      container.appendChild(div);
+    } 
+  } else {
+    alert('Please enter number between 1 and 100');
+    
+    gridInit();
+  }
 });
